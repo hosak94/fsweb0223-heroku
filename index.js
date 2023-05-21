ff5a820a793ff0b740191cdbf3832b28751885b1
hosak94 <yigitcanhosak@gmail.com>
2023-05-21 17:21:59 +0300

    proje setupı yapıldı

---
+++ index.js
@@ -0,0 +1,14 @@
+/* const jazz = process.argv[4];
+console.log("third", jazz);
+ */
+require("dotenv").config();
+
+const server = require("./api/server");
+const { PORT, NODE_ENV } = require("./config/config");
+
+console.log("PORT:", PORT);
+console.log("NODE_ENV:", NODE_ENV);
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
